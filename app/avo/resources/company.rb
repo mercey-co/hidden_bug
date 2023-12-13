7c7b90cc600cc4ca1af214aeb89ba4357448e302
Jean-Etienne Durand <etienne@ultraestates.com>
2023-12-13 08:48:15 +0100

    Reproduce tabs bug

--- app/avo/resources/company.rb
+++ app/avo/resources/company.rb
@@ -5,7 +5,21 @@ class Avo::Resources::Company < Avo::BaseResource
   # }
 
   def fields
-    field :id, as: :id
-    field :name, as: :text
+    main_panel do
+      field :id, as: :id
+      field :name, as: :text
+    end
+    tabs id: 'whatever' do
+      tab 'Tab1' do
+        panel 'Foo' do
+          field :name, as: :text
+        end
+      end
+      tab 'Tab2' do
+        panel 'Foo' do
+          field :description, as: :text
+        end
+      end
+    end
   end
 end
